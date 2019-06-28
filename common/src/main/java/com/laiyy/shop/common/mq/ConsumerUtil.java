f559939d2f81abe1e806febf73ac62593df73354
laiyy0728 <laiyy0728@gmail.com>
2019-06-28 16:15:24 +0800

    init

---
+++ common/src/main/java/com/laiyy/shop/common/mq/ConsumerUtil.java
@@ -0,0 +1,9 @@
+package com.laiyy.shop.common.mq;
+
+/**
+ * @author laiyy
+ * @date 2019/6/28 16:14
+ * description：RocketMQ 消费者
+ */
+public class ConsumerUtil {
+}
---
+++ common/src/main/java/com/laiyy/shop/common/mq/ProducerUtil.java
@@ -0,0 +1,13 @@
+package com.laiyy.shop.common.mq;
+
+/**
+ * @author laiyy
+ * @date 2019/6/28 16:13
+ * description： RocketMQ 生产者
+ */
+public class ProducerUtil {
+
+
+
+
+}
